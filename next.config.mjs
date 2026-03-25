59595bf29a59f75baa0c9d7ec46383fc672ad1d1
The One Who Overcame Adversity <nico.par@ciit.edu.ph>
2026-03-25 22:17:38 +0800

    deploy bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import { setupPlatform } from '@cloudflare/next-on-pages';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
 };
 
-const withCloudflare = setupPlatform();
-export default withCloudflare(nextConfig);
+export default nextConfig;
